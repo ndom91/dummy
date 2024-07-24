7ba1663d0c28c788ca7f2f852b6f635f72638af8
ndom91 <yo@ndo.dev>
2024-07-24 10:39:45 +0200

    New Commits Test 12345
    New RegExp

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ console.log(new URL(b))
 const c = new RegExp()
 
 console.log('regexp', c)
+const b = window.location
+
+console.log(new URL(b))
+
+const c = new RegExp()
+
+console.log('regexp', c)
