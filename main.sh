3c5ec83afa42c0331529a24b1479c1b792d78697
ndom91 <yo@ndo.dev>
2024-11-20 16:44:22 +0100

    commit.3

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 function reverse_string() {
         # Read input string
-        read -p "Enter a string: " string
+        read -p "Enter string: " string
         # Reverse the string
         reversed_string=$(echo $string | rev)
         # Print the reversed string
